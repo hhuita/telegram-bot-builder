5ea7b3e0ed484593a279cf37249d3d009a479c68
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-05-05 03:14:34 +0300

    auto: agent changes

--- migrations/0023_add_user_tracking_fields.sql
+++ migrations/0023_add_user_tracking_fields.sql
@@ -1,6 +1,6 @@
 -- @fileoverview Миграция: добавление полей трекинга пользователей в таблицу bot_users
 -- Добавляет поля is_premium, is_bot, language_code, deep_link_param, referrer_id
--- Created: 2026-02-22
+-- Created: 2026-05-05
 
 ALTER TABLE bot_users ADD COLUMN IF NOT EXISTS is_premium integer DEFAULT 0;
 ALTER TABLE bot_users ADD COLUMN IF NOT EXISTS is_bot integer DEFAULT 0;
